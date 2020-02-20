7557ddf34ca5ef3f65d1bd05649535c4388bcbf8
Ivan Stoiev <ivan.stoiev@cobli.co>
2020-02-20 14:58:35 -0300

    add incident update action

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,10 @@
+import json
+
 import pytest
 import responses
 from flask import Flask, template_rendered
 from flask.testing import FlaskClient
+from requests import PreparedRequest
 
 from statuspage2slack import create_app
 
@@ -20,9 +23,19 @@ def flask_client(flask_app) -> FlaskClient:
 
 
 @pytest.fixture
-def request_mocker():
+def request_mocker(flask_app):
     with responses.RequestsMock(
             assert_all_requests_are_fired=False) as req_mocker:
+        def assert_request(request: PreparedRequest):
+            assert request.body
+            assert is_json(request.body)
+
+            return 200, {}, 'ok'
+
+        req_mocker.add_callback(responses.POST,
+                                flask_app.config.get('SLACK_WEBHOOK_URL'),
+                                callback=assert_request)
+
         yield req_mocker
 
 
@@ -38,3 +51,11 @@ def used_templates(flask_app):
         yield recorded
     finally:
         template_rendered.disconnect(record, flask_app)
+
+
+def is_json(some_string):
+    try:
+        json.loads(some_string)
+    except ValueError:
+        return False
+    return True
